8e907a235f7fc2ebf5cfd8e5019322d10f54b6da
Kevin Blaszczyk <OrcHound@gmail.com>
2020-10-27 21:42:33 +0100

    Render the board into an image

--- src/main/java/de/orchound/gameoflife/rendering/BoardRenderer.java
+++ src/main/java/de/orchound/gameoflife/rendering/BoardRenderer.java
@@ -2,6 +2,8 @@ package de.orchound.gameoflife.rendering;
 
 import de.orchound.gameoflife.game.Board;
 import processing.core.PApplet;
+import processing.core.PConstants;
+import processing.core.PImage;
 
 public class BoardRenderer {
 
@@ -11,39 +13,38 @@ public class BoardRenderer {
 	private final float halfBoardWidth;
 	private final float halfBoardHeight;
 
+	private final PImage boardRepresentation;
+	private final int setColor;
+	private final int unsetColor;
+
 	public BoardRenderer(Board board, PApplet sketch) {
 		this.board = board;
-		this.sketch = sketch;
-
 		halfBoardWidth = board.width / 2f;
 		halfBoardHeight = board.height / 2f;
+
+		this.sketch = sketch;
+		setColor = sketch.color(115, 210, 22);
+		unsetColor = sketch.color(0);
+
+		boardRepresentation = sketch.createImage(board.width, board.height, PConstants.RGB);
+		update();
 	}
 
 	public void render() {
-		sketch.pushStyle();
-		sketch.strokeWeight(0f);
+		sketch.image(boardRepresentation, -halfBoardWidth, -halfBoardHeight);
+	}
 
+	public void update() {
 		boolean[][] data = board.target;
-		for (int i = 0; i < data.length; i++) {
-			for (int j = 0; j < data[0].length; j++) {
-				if (data[i][j]) {
-					drawCell(i, j);
-				}
+
+		boardRepresentation.loadPixels();
+
+		for (int i = 0; i < board.height; i++) {
+			for (int j = 0; j < board.width; j++) {
+				boardRepresentation.pixels[i * board.width + j] = data[i][j] ? setColor : unsetColor;
 			}
 		}
 
-		sketch.popStyle();
-	}
-
-	private void drawCell(int x, int y) {
-		sketch.pushMatrix();
-		sketch.pushStyle();
-
-		sketch.translate(y - halfBoardWidth, x - halfBoardHeight);
-		sketch.fill(115f, 210f, 22f);
-		sketch.rect(0.05f, 0.05f, 0.9f, 0.9f);
-
-		sketch.popStyle();
-		sketch.popMatrix();
+		boardRepresentation.updatePixels();
 	}
 }
