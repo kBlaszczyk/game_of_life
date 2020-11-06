f5314bf7599a806680e3616210a9868753542820
Kevin Blaszczyk <OrcHound@gmail.com>
2020-11-06 17:26:36 +0100

    Implement basic slider

---
+++ src/main/java/de/orchound/gameoflife/processing/Slider.java
@@ -0,0 +1,47 @@
+package de.orchound.gameoflife.processing;
+
+import org.joml.Vector2f;
+import org.joml.Vector2fc;
+import processing.core.PApplet;
+
+import java.util.function.Consumer;
+
+public class Slider {
+
+	private final Vector2fc position;
+	private final Vector2fc size = new Vector2f(60, 30);
+	private float value = 0.5f;
+	private final float handleWidth = 8f;
+
+	private final PApplet sketch;
+	private final Consumer<Float> action;
+
+	public Slider(float x, float y, PApplet sketch, Consumer<Float> action) {
+		position = new Vector2f(x, y);
+
+		this.sketch = sketch;
+		this.action = action;
+	}
+
+	public void draw() {
+		sketch.pushStyle();
+
+		sketch.rect(position.x(),position.y(), size.x(), size.y());
+		final float handleX = position.x() + value * (position.x() + size.x() - position.x());
+
+		sketch.fill(0);
+		final float handleHalfWidth = handleWidth / 2f;
+		sketch.rect(handleX - handleHalfWidth, position.y(), handleWidth, size.y());
+
+		sketch.popStyle();
+	}
+
+	public void drag(float x, float y) {
+		float newValue = PApplet.map(x, position.x(), position.x() + size.x(), 0f, 1f);
+		action.accept(Math.min(1f, Math.max(0f, newValue)));
+	}
+
+	public void setValue(float newValue) {
+		value = newValue;
+	}
+}
